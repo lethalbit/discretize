b298e513034454aae5a9d7718cdfe9eb1582524f
Aki Van Ness <aki@lethalbit.net>
2023-11-18 14:02:36 -0800

    techlib: added some stub DFF/latch modules

---
+++ src/techlib/cells_dffe.v
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+(* techmap_celltype = "$_DLATCH_P_" *)
+module _80_FET_dlatch_p (CLK, D, Q);
+	input  CLK;
+	input  D;
+	output Q;
+
+endmodule
+
+(* techmap_celltype = "$_DLATCH_N_" *)
+module _80_FET_dlatch_n (CLK, D, Q);
+	input  CLK;
+	input  D;
+	output Q;
+
+endmodule
+
+
+(* techmap_celltype = "$_DFF_N_" *)
+module _80_FET_dff_n (CLK, D, Q);
+	input  CLK;
+	input  D;
+	output Q;
+
+
+endmodule
+
+(* techmap_celltype = "$_DFF_P_" *)
+module _80_FET_dff_p (CLK, D, Q);
+	input  CLK;
+	input  D;
+	output Q;
+
+endmodule
